ccc5198329bbde10e9d7d63a1a0abcecfba2a8f6
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-04-16 17:10:15 +0900

    exercise pipeline

--- src/interface/ExercisePipeline.ts
+++ src/interface/ExercisePipeline.ts
@@ -1,1 +1,9 @@
-export default interface ExercisePipeline {}
+import ExerciseClassfier from './ExerciseClassfier';
+import JointPosition from './JointPosition';
+import Preprocesser from './Preprocesser';
+
+export default interface ExercisePipeline {
+  setJointPosition(pos: JointPosition): Promise<void>;
+  setPreprocesser(pro: Preprocesser): void;
+  setClassfier(cla: ExerciseClassfier): Promise<void>;
+}
